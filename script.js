9491899f7d888f84289317cecf7e1a980bc91b7d
Luca Versari <versariluca00@gmail.com>
2024-07-06 11:35:53 +0200

    added git

---
+++ script.js
@@ -0,0 +1,16 @@
+const ex1 = document.querySelector(".ex1")
+const ex2 = document.querySelector(".ex2")
+const ex3 = document.querySelector(".ex3")
+const ex4 = document.querySelector(".ex4")
+const ex5 = document.querySelector(".ex5")
+const ex6 = document.querySelector(".ex6")
+const ex7 = document.querySelector(".ex7")
+const ex8 = document.querySelector(".ex8")
+const ex9 = document.querySelector(".ex9")
+const ex10 = document.querySelector(".ex10")
+const ex11 = document.querySelector(".ex11")
+const ex12 = document.querySelector(".ex12")
+
+ex1.textContent = "Exercise 1"
+ex2.textContent = "Exercise 2"
+ex3.textContent = "Exercise 3"
